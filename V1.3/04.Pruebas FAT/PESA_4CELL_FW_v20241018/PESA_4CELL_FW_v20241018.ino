068b98bc203b345d17d3a8ac2b701674a6d68b81
franciscoVegaC <francisco.vega@ceaza.cl>
2025-05-15 11:35:53 -0400

    licencias txt

--- V1.3/04.Pruebas FAT/PESA_4CELL_FW_v20241018/PESA_4CELL_FW_v20241018.ino
+++ V1.3/04.Pruebas FAT/PESA_4CELL_FW_v20241018/PESA_4CELL_FW_v20241018.ino
@@ -1,25 +1,3 @@
-/*MIT License
-
-Copyright (c) 2025 Centro de Estudios Avanzados en Zonas Áridas
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.*/
-
 /*El presente Sketch corresponde al Firmware de la
  * unidad remota (RTU) asociada al peso de nieve
  * para ser enlazada por medio de comunicación serial
@@ -46,7 +24,6 @@ SOFTWARE.*/
  * Firmware escrito por Adrián Gallardo, CEAZA.
  * 
  * V20211006.1: Se adapta para medición de solo una celda en el canal A
- 
  * V20221128.1: se adapta cógigo para unidad con 2 HX711
  * V20221213.1: se corrige implementación de tara, se muestran 7 decimales de valores proporcionales de calibración al ser consultados
  * V20221227.1: se corrige calculo de tara
